8ca5d3c49ff3afa37d4e1b975f94355c3102a07f
Vlada Mitic <rokagresor@gmail.com>
2021-08-01 14:51:35 +0200

    adding patch method i access control header

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use((req, res, next) => {
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept'
   );
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,PATCH,POST,DELETE');
   next();
 });
 
