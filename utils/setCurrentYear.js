e0d67e41410ac46ce89e06035b010f98e2523043
Vlada Mitic <rokagresor@gmail.com>
2021-09-12 21:51:08 +0200

    fixing count current year papers route

--- utils/setCurrentYear.js
+++ utils/setCurrentYear.js
@@ -1,5 +1,5 @@
 exports.setCurrentYear = (req, res, next) => {
   const yearNow = new Date(Date.now()).getFullYear();
-  req.query.createdAt = { $gte: new Date(`${yearNow}-01-01`) };
+  req.query.createdAt = { gte: new Date(`${yearNow}-01-01`) };
   next();
 };
